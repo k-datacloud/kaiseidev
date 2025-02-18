c7ec7bd27a15c9040f642e77ec0f0a079ef1ee4c
Kasiei <k25ts09@outlook.jp>
2025-02-18 23:53:19 +0900

    code commits

--- main.js
+++ main.js
@@ -2,14 +2,30 @@
 
 const nav = document.querySelector(".header__nav");
 const iconBtn = document.querySelector(".header__icon");
+const menuItem = document.querySelectorAll(".menu-item--close");
 const menuImage = document.querySelector(".header__icon img");
 iconBtn.addEventListener("click", () => {
+    //image change
     if ( menuImage.getAttribute("src") === "assets/close.svg" ) {
-        nav.classList.add("show");
         menuImage.setAttribute("src", "assets/open.svg");
     } else if ( menuImage.getAttribute("src") === "assets/open.svg" ) {
-        nav.classList.remove("show");
         menuImage.setAttribute("src", "assets/close.svg");
+    }
+    //nav show
+    nav.classList.toggle("show");
+
+    for (let i = 0; i < menuItem.length; i++) {
+        if ( menuItem[i].classList.contains("menu-item--close") ) {
+            menuItem.forEach((item, index) => {
+                setTimeout(() => {
+                    item.classList.remove("menu-item--close");
+                    item.classList.add("menu-item--open");
+                }, index * 100)
+            })
+        } else {
+            menuItem[i].classList.remove("menu-item--open");
+            menuItem[i].classList.add("menu-item--close");
+        }
     }
 })
 
@@ -37,27 +53,26 @@ quesionts.forEach((question) => {
 
 // exchange language
 const langSwitchBtn = document.querySelector(".language-switch");
-const questionTexts = document.querySelector(".question");
+const questionTexts = document.querySelectorAll(".question");
 langSwitchBtn.addEventListener("click", () => {
-
-    if (questionTexts.textContent.trim() === questionTexts.getAttribute("data-jp")) {
-        questionTexts.classList.add("question--en");
-        answers[0].classList.add("answer--en");
-        questionTexts.textContent = questionTexts.getAttribute("data-en");
-        answers[0].textContent = answers[0].getAttribute("data-en");
-    } else {
-        questionTexts.textContent = questionTexts.getAttribute("data-jp");
-        answers[0].textContent = answers[0].getAttribute("data-jp");
-    }
     
-    // questionTexts.forEach((questionText) => {
-    //     if ( questionText.textContent == questionText.getAttribute("data-jp") ) {
-    //         questionText.textContent = questionText.getAttribute("data-en");
-    //     } else {
-    //         console.log("else");
-            
-    //     }
-    // })
+    questionTexts.forEach((questionText) => {
+        if ( questionText.textContent.trim() == questionText.getAttribute("data-jp") ) {
+            questionText.textContent = questionText.getAttribute("data-en");
+            questionText.classList.add("question--en");
+            answers.forEach((answer) => {
+                answer.textContent = answer.getAttribute("data-en");
+                answer.classList.add("answer--en");
+            })
+        } else {
+            questionText.textContent = questionText.getAttribute("data-jp");
+            questionText.classList.remove("question--en");
+            answers.forEach((answer) => {
+                answer.textContent = answer.getAttribute("data-jp");
+                answer.classList.remove("answer--en");
+            })
+        }
+    })
 })
 
 // footerText show
